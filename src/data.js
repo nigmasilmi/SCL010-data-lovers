2b7080a1ff2bf0550c1143783f356c2a65863e45
anabelcarrion <anabelcarrion3110@gmail.com>
2019-07-05 16:27:32 -0400

    Mostrar tablas con valores, segun el medio de tranporte

--- src/data.js
+++ src/data.js
@@ -1,58 +1,93 @@
-/* Manejo de data 
 
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
+let dataModified = new Map();
 
-const example = () => {
-  return 'example';
-};
-
-window.example = example;*/
-
-//Haciendo lista de la Data
- var dataModified = new Map();
- var container=[];
-
- function loadData(){
- let year=null;
+function loadData(){
   
-  for (let i=0; i<INJURIES.length; i++){
+    for (let i=0; i<INJURIES.length; i++){
   
-    year= INJURIES[i].Year.substring(0,4);
+    let year= INJURIES[i].Year.substring(0,4);
      
     //data para transporete aereo
-    let air= 
-      {"Total_Injured_Persons_Air":INJURIES[i].Total_Injured_Persons_Air,
-      "Total_Injured_Persons_US_Air_Carrier":INJURIES[i].Total_Injured_Persons_US_Air_Carrier,
-      "Total_Injured_Persons_General_Aviation":INJURIES[i].Total_Injured_Persons_General_Aviation,
-      "Total_Injured_Persons_On_Demand_Air_Taxi":INJURIES[i].Total_Injured_Persons_On_Demand_Air_Taxi};
-    
+    let air= [
+      {
+        "name": "Total_Injured_Persons_Air",
+        "nameSpanish":"Total de personas lesionadas en transporte aereo",
+        "value":INJURIES[i].Total_Injured_Persons_Air
+      },
+      {
+        "name":"Total_Injured_Persons_On_Demand_Air_Taxi",
+        "nameSpanish":"Total de personas lesionadas en aviacion tipo taxis",
+        "value":INJURIES[i].Total_Injured_Persons_On_Demand_Air_Taxi
+      },
+      {
+        "name":"Total_Injured_Persons_US_Air_Carrier",
+        "nameSpanish":"Total de personas lesionadas en Aerolínea Estadounidense",
+        "value":INJURIES[i].Total_Injured_Persons_US_Air_Carrier
+      },
+      {
+        "name":"Total_Injured_Persons_General_Aviation",
+        "nameSpanish":"Total de personas lesionadas en Aerolínea no Estadounidense",
+        "value":INJURIES[i].Total_Injured_Persons_General_Aviation
+      }];
 
     //data para transporete terrestre
-    let land=
-      {"Total_Injured_Persons_Pedalcyclists":INJURIES[i].Total_Injured_Persons_Pedalcyclists,
-      "Total_Injured_Persons_Motorcyclists":INJURIES[i].Total_Injured_Persons_Motorcyclists,
-      "Total_Injured_Persons_Passenger_Car_Occupants":INJURIES[i].Total_Injured_Persons_Passenger_Car_Occupants,
-      "Total_Injured_Persons_Bus_Occupants":INJURIES[i].Total_Injured_Persons_Bus_Occupants,
-      "Total_Injured_Persons_Rail":INJURIES[i].Total_Injured_Persons_Rail};
+    let land=[
+    {
+      "name":"Total_Injured_Persons_Pedalcyclists",
+      "nameSpanish":"Total de personas lesionadas en bicicleta",
+      "value":INJURIES[i].Total_Injured_Persons_Pedalcyclists
+    },
 
+    {
+      "name":"Total_Injured_Persons_Motorcyclists",
+      "nameSpanish":"Total de personas lesionadas en motocicleta",
+      "value":INJURIES[i].Total_Injured_Persons_Motorcyclists
+    },
+    {
+      "name":"Total_Injured_Persons_Passenger_Car_Occupants",
+      "nameSpanish":"Total de personas lesionadas ocupamtes de un carro",
+      "value":INJURIES[i].Total_Injured_Persons_Passenger_Car_Occupants
+    },
+    {
+      "name":"Total_Injured_Persons_Bus_Occupants",
+      "nameSpanish":"Total de personas lesionadas ocupamtes de un bus",
+      "value":INJURIES[i].Total_Injured_Persons_Bus_Occupants
+    },
+    {
+      "name":"Total_Injured_Persons_Rail",
+      "nameSpanish":"Total de personas lesionadas ocupamtes de un tren",
+      "value":INJURIES[i].Total_Injured_Persons_Rail
+    }];
     //data para transporete terrestre
-    let water=
-     {"Total_Injured_Persons_Passenger_Vessel":INJURIES[i].Total_Injured_Persons_Passenger_Vessel,
-     "Total_Injured_Persons_Recreational_Boating":INJURIES[i].Total_Injured_Persons_Recreational_Boating,
-     "Total_Injured_Persons_Water_Vessel_Related":INJURIES[i].Total_Injured_Persons_Water_Vessel_Related,
-      "Total_Injured_Persons_Water":INJURIES[i].Total_Injured_Persons_Water};33.
+    let water=[
+      {
+        "name":"Total_Injured_Persons_Passenger_Vessel",
+        "nameSpanish":"Total de personas lesionadas ocupantes de embarcaciones",
+        "value":INJURIES[i].Total_Injured_Persons_Passenger_Vessel
+      },
+      {
+        "name":"Total_Injured_Persons_Recreational_Boating",
+        "nameSpanish":"Total de personas lesionadas en embarcaciones de recreo",
+        "value":INJURIES[i].Total_Injured_Persons_Recreational_Boating
+      },
+      {
+        "name":"Total_Injured_Persons_Water_Vessel_Related",
+        "nameSpanish":"Total de personas lesionadas en embarcaciones de recreo",
+        "value":INJURIES[i].Total_Injured_Persons_Water_Vessel_Related
+      },
+      
+      {
+        "name":"Total_Injured_Persons_Water",
+        "nameSpanish":"Total de personas lesionadas en embarcaciones de recreo",
+        "value":INJURIES[i].Total_Injured_Persons_Water
+      }];
+      
+      
+      dataModified.set(year,{water,air,land});
+    } 
+  } 
+   
     
-     
-      dataModified.set(year,{
-        water,
-        air,
-        land
-      });
-     //container.push(dataModified);
-     // console.log(container);
-     //console.log(dataModified);
-     //console.log(year); 
-  }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
-}
+ 
+
 
---
+++ src/main.js
@@ -0,0 +1,47 @@
+
+
+const containerDatos = document.getElementById("container_datos");
+document.getElementById("clickme").addEventListener("click", (showTable));
+
+//mostrar tabla
+function showTable(){ 
+    let transport = document.body.id;
+    let years = document.getElementById("selectYear").value;
+    drawTable(years,transport);
+}
+
+//seleccion de rango de tiempo
+function changeYearsToList(years){
+    let year = [];
+    for (let i = 0; i < years.length; i=i+5) {
+        year.push(years.substring(i,i+4));        
+    }
+    return year;
+}
+ 
+//crear tablas
+function drawTable(year,transport){  
+ loadData();
+ year = changeYearsToList(year);
+ let rows="";
+
+ for (let i=0; i<dataModified.get(year[0])[transport].length; i++){
+
+    rows +="<tr>"+"<td>"+ dataModified.get(year[0])[transport][i].nameSpanish +"</td>";
+       
+    for (let j=0; j<year.length; j++){
+
+       rows += "<td>"+ dataModified.get(year[j])[transport][i].value +"</td>";
+    }
+   rows += "<tr>";
+}
+
+   let tabla = "<table>"+  "<tr>"+  "<th>"+"Incidentes"+"</th>";
+ for (let i = 0; i < year.length; i++) {
+     tabla+="<th>"+year[i]+"</th>";
+     
+ } 
+ tabla+= "</tr>"+rows+"</table>";
+
+ containerDatos.innerHTML = tabla;
+ }
